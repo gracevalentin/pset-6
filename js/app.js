c58cbdc6e83a51e96f259a07522f756f1e18fc2d
gracevalentin <55455941+gracevalentin@users.noreply.github.com>
2020-01-07 09:09:24 -0500

    updated array

--- js/app.js
+++ js/app.js
@@ -7,9 +7,8 @@ var priorityButton = document.getElementsByClassName("priority")
 const todoItems = [];
 
 //creating to-dos function
-
+var li = document.createElement('li');
 function createNewElement(){
-    var li = document.createElement('li');
     li.id = todoItems.length;
     var theInputValue = document.getElementById("the-input").value;
 
@@ -30,40 +29,42 @@ function createNewElement(){
       document.getElementById("the-ul").appendChild(li);
     }
 
+//checked
+var ulList = document.querySelector('ul')
+ulList.addEventListener('click', function(event){
+  if(event.target.tagName === "LI"){
+    event.target.classList.toggle('checked');
+  }
+})
 
-  //document.getElementById("the-input").value = "";
+//removed
+document.getElementById("the-input").value = "";
 
-
-    var thePanTag = document.createElement("SPAN");
-    var xMark = document.createTextNode("\u00d7");
-    thePanTag.className = "close";
-    thePanTag.appendChild(xMark);
-    li.appendChild(thePanTag)
-
-
-stuff below doesnt work!!
-
-    var exclamationPoint = document.createTextNode("!");
-    thePanTag.className = "priority";
-    thePanTag.appendChild(exclamationPoint);
-    li.appendChild(thePanTag)
+  var thePanTag = document.createElement("SPAN");
+  var xMark = document.createTextNode("\u00d7");
+  thePanTag.className = "close";
+  thePanTag.appendChild(xMark);
+  li.appendChild(thePanTag)
 
 //removes items when clicked on span close button
 
-    for (i = 0; i < closeButton.length; i++) {
-     closeButton[i].onclick = function() {
-       var theDiv = this.parentElement;
-       console.log(theDiv.id);
-       todoItems.splice(theDiv.id, 1);
 
-       theDiv.style.display = "none";
-     }
-    }
-}
+  for (i = 0; i < closeButton.length; i++) {
+   closeButton[i].onclick = function() {
+     var theDiv = this.parentElement;
+     todoItems.splice(theDiv.id, 1);
+     theDiv.style.display = "none";
+   }
+  }
 
-  var ulList = document.querySelector('ul')
-  ulList.addEventListener('click', function(event){
-    if(event.target.tagName === "LI"){
-      event.target.classList.toggle('checked');
-    }
-  });
+};
+
+
+//array
+
+let index = todoItems.indexOf(todoItems);
+
+  todoItems[index] = document.createElement("li");
+  document.getElementById("ul").append(ulList[index].li);
+
+//array end
