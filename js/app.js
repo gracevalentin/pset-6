5c4819da8970945fbd1f5ee2b92116f7b5c5e8ff
gracevalentin <55455941+gracevalentin@users.noreply.github.com>
2020-01-07 18:26:39 -0500

    almost done :D

--- js/app.js
+++ js/app.js
@@ -1,3 +1,7 @@
+window.onload = function() {
+  document.getElementById("add-button").onclick = addItem;
+}
+
 //close and priority buttons
 
 var closeButton = document.getElementsByClassName("close")
@@ -6,65 +10,90 @@ var priorityButton = document.getElementsByClassName("priority")
 
 const todoItems = [];
 
-//creating to-dos function
-var li = document.createElement('li');
-function createNewElement(){
-    li.id = todoItems.length;
-    var theInputValue = document.getElementById("the-input").value;
+// add new items to array
 
+function addItem() {
+  const input = document.getElementById("the-input").value;
 
-    todoItems.push({
-      id: todoItems.length,
-      content: theInputValue,
-      priority: "low",
-      complete: false
-    });
+  todoItems.push({
+    id: todoItems.length,
+    content: input,
+    priority: "low",
+    complete: false
+  });
+  document.getElementById("the-input").value = "";
 
+  renderItems();
+}
 
-    var textNode = document.createTextNode(theInputValue);
-    li.appendChild(textNode);
-    if(theInputValue == ''){
-      alert("This cannot be left empty!");
-    } else{
-      document.getElementById("the-ul").appendChild(li);
-    }
+function renderItems() {
+  const list = document.getElementById("the-ul");
+  list.innerHTML = "";
 
-//checked
-var ulList = document.querySelector('ul')
-ulList.addEventListener('click', function(event){
-  if(event.target.tagName === "LI"){
-    event.target.classList.toggle('checked');
+  for (let i = 0; i < todoItems.length; i++) {
+    const li = document.createElement("li");
+    const priority = document.createElement("span");
+    const content = document.createElement("span");
+    const remove = document.createElement("span");
+
+    li.id = i;
+    li.onclick = toggleCompletion;
+    li.className = todoItems[i].complete === true ? "checked" : "";
+
+    priority.id = i;
+    priority.innerHTML = "!";
+    priority.className = "priority";
+    priority.onclick = togglePriority;
+
+    content.id = i;
+    content.innerHTML = todoItems[i].content;
+
+    remove.id = i;
+    remove.innerHTML = "\u00d7";
+    remove.className = "close";
+    remove.onclick = removeItem;
+
+    li.appendChild(priority);
+    li.appendChild(content);
+    li.appendChild(remove);
+    list.appendChild(li);
   }
-})
+}
 
-//removed
-document.getElementById("the-input").value = "";
+function togglePriority() {
+  const index = this.id;
 
-  var thePanTag = document.createElement("SPAN");
-  var xMark = document.createTextNode("\u00d7");
-  thePanTag.className = "close";
-  thePanTag.appendChild(xMark);
-  li.appendChild(thePanTag)
+  if (todoItems[index].priority === "low") {
+    todoItems[index].priority = "high";
 
-//removes items when clicked on span close button
+    const item = todoItems.splice(index, 1)[0];
+    todoItems.unshift(item);
+  } else {
+    todoItems[index].priority = "low";
 
-
-  for (i = 0; i < closeButton.length; i++) {
-   closeButton[i].onclick = function() {
-     var theDiv = this.parentElement;
-     todoItems.splice(theDiv.id, 1);
-     theDiv.style.display = "none";
-   }
+    const item = todoItems.splice(index, 1)[0];
+    todoItems.push(item);
   }
 
-};
+  renderItems();
+}
 
+function toggleCompletion() {
+  console.log(this);
+  const index = this.id;
 
-//array
+  if (todoItems[index].complete === false) {
+    todoItems[index].complete = true;
+  } else {
+    todoItems[index].complete = false;
+  }
 
-let index = todoItems.indexOf(todoItems);
+  renderItems();
+}
 
-  todoItems[index] = document.createElement("li");
-  document.getElementById("ul").append(ulList[index].li);
+function removeItem() {
+  const index = this.id;
+  todoItems.splice(index, 1);
 
-//array end
+  renderItems();
+}
